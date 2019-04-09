219b609daa922d7b59fe05fbbbb9018bf02eb72e
Kasper Sørensen <ksare17@student.aau.dk>
2019-04-09 09:24:28 +0200

    +bugfixes, +samplingfunction

--- P4_AP.m
+++ P4_AP.m
@@ -1,28 +1,44 @@
 %% AudioProcessing
-recObj = audiorecorder;
+recObj = audiorecorder(44100, 8, 1);
+ 
 
-while(true)
+
 disp('Start speaking.');
-recordblocking(recObj, 10);
+recordblocking(recObj, 1);
 disp('End of Recording.');
 
 
-data = getaudiodata(recObj);
+data = getaudiodata(recObj, 'double');
+
+% if data < data.length 
+%    for (a[i] = 0; i*1323 < data; i++) {
+%        testData = data(1323*i:1323*i+1323);
+%        }
+%    
+% end
+testArr = zeros(1323,1);
+for i = 0:44100/1323
+   if i < 33
+    test = data(i*1323+1:i*1323+1323); 
+    testArr = [testArr test];
+   end
+end
 data_fft = fft(data);
 subplot(2,1,2)
+%plot(data_fft)
 plot(abs(data_fft(:,1)));
 audioInMono = mean(data,2);
-ylabel('Magnitude')
-xlabel('Frequency Values')
 
 t = (0:length(audioInMono)-1)/44100;
 subplot(2,1,1)
-plot(t,audioInMono)
-
-ylabel('Amplitude') 
+%0p0lot(t,audioInMono)
 ylabel('Amplitude')
-xlabel('Time')
-end
+fid=fopen('StressTestSheet.txt', 'w');
+fmt = '%5d %5d %5d %5d\n';
+fprintf(fid,fmt,data_fft);
+fclose(fid);
+
+
 
 
 %% Supervised learning
