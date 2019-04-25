cedd53401ca2e7bc18a2201c3218bcecdaa823f8
HamPaa35 <mfaldt17@student.aau.dk>
2019-04-25 14:13:20 +0200

    The code now loops through all the models, and all the logliks need to figure out the best way to get the correct word

--- P4_AP.m
+++ P4_AP.m
@@ -1,8 +1,8 @@
 %% AudioProcessing
 addpath(genpath("./audio"))
-[Help1,Fs] = audioread('10_hjï¿½lp_Mathias.wav');
+[Help1,Fs] = audioread('10_hjælp_Mathias.wav');
 [Help2,Fs] = audioread('help-Glerup.wav');
-[Help3,Fs] = audioread('Hjï¿½lp_Rasmus.wav');
+[Help3,Fs] = audioread('Hjælp_Rasmus.wav');
 [Help4,Fs] = audioread('10_helps_Female.wav');
 [skrig1,Fs] = audioread('10_skrig_Mathias.wav');
 [skrig2,Fs] = audioread('screech-Glerup.wav');
@@ -15,19 +15,19 @@ framesize = 30/1000*fs;
 
 % Assumed implementation of loop : help
 % move into function later
-h = [];
+% h = [];
 % loop through folder with different people
-for i = 0:foldersize1
-    foldersize2 = size(folder, i);
-    % loop through folder of different sound files and add parameters to
-    % list.
-    for j = 0:foldersize2
-        [Help,Fs] = audioread('file[j]');
-        h[i] = append(h[i], audioProcessing(Help, Fs));
-    end
-    % create different combinations of parameters
-
-end
+% for i = 0:foldersize1
+%     foldersize2 = size(folder, i);
+%     % loop through folder of different sound files and add parameters to
+%     % list.
+%     for j = 0:foldersize2
+%         [Help,Fs] = audioread('file[j]');
+%         h[i] = append(h[i], audioProcessing(Help, Fs));
+%     end
+%     % create different combinations of parameters
+% 
+% end
 
 % Do AP on help signals
 h1 = audioProcessing(Help1, Fs);
@@ -45,6 +45,8 @@ s4 = audioProcessing(skrig4, Fs);
 
 AllS = {s1, s2, s3, s4};
 
+AllaudioData = [AllH; AllS];
+
 disp("done")
 %% Train Gmm-Hmm model
 % Locates files
@@ -56,9 +58,13 @@ M = 3;      % mix num
 [p_start, A, phi, loglik] = ChmmGmm(AllH, Q, M);
 disp("hdone")
 [tp_start, tA, tphi, tloglik] = ChmmGmm(AllS, Q, M);
-disp("Trained")
+disp("Trained") 
+% HelpModel = Model(p_start, A, phi);
+% ScreamModel = Model(tp_start, tA, tphi);
 model_1h = {p_start, A, phi};
 model_2h = {tp_start, tA, tphi};
+%% test zone
+GmmModels = trainModels(AllaudioData);
 %% Eval model vs. data
 [HelpTest,Fs] = audioread('Skrig_Rasmus.wav');
 [skrigTest,Fs] = audioread('test_skrig.mp3');
@@ -67,7 +73,7 @@ recObj = audiorecorder(Fs, 8, 2);
 disp('Start speaking.');
 recordblocking(recObj, 2);
 disp('End of Recording.');
-%
+
 liveData = getaudiodata(recObj, 'double');
 Live = {audioProcessing(liveData, Fs)};
 
@@ -75,15 +81,17 @@ ht = {audioProcessing(HelpTest, Fs)};
 
 st = {audioProcessing(skrigTest, Fs)};
 
-CalOfLoglik(ht, model_1h, model_2h)
+testting = evalModels(Live, GmmModels);
+
+CalOfLoglik(Live, GmmModels{1}, GmmModels{2})
 
 function CalOfLoglik(evalData, model_l, model_2)
     obj_num = length(evalData);
     for r = 1:obj_num
-        logp_xn_given_zn = Gmm_logp_xn_given_zn(evalData{r}, model_l{3});
-        [~, ~, Loglik1] = LogForwardBackward(logp_xn_given_zn, model_l{1}, model_l{2});
-        logp_xn_given_znt = Gmm_logp_xn_given_zn(evalData{r}, model_2{3});
-        [~, ~, Loglik2] = LogForwardBackward(logp_xn_given_znt, model_2{1}, model_2{2});
+        logp_xn_given_zn = Gmm_logp_xn_given_zn(evalData{r}, model_l.phi);
+        [~, ~, Loglik1] = LogForwardBackward(logp_xn_given_zn, model_l.p_start, model_l.A);
+        logp_xn_given_znt = Gmm_logp_xn_given_zn(evalData{r}, model_2.phi);
+        [~, ~, Loglik2] = LogForwardBackward(logp_xn_given_znt, model_2.p_start, model_2.A);
     end
     disp(Loglik1)
     disp(Loglik2)
@@ -95,14 +103,16 @@ function CalOfLoglik(evalData, model_l, model_2)
     end
 end
 
-function CalOfLoglik2(evalData, model_1_phi, model_2_phi, model_1_p_start, model_2_p_start, model_1_A, model_2_A)
+function CalOfLoglik2(evalData, model_l, model_2)
     obj_num = length(evalData);
     for r = 1:obj_num
-        logp_xn_given_zn = Gmm_logp_xn_given_zn(evalData{r}, model_1_phi);
-        [~, ~, Loglik1] = LogForwardBackward(logp_xn_given_zn, model_1_p_start, model_1_A);
-        logp_xn_given_znt = Gmm_logp_xn_given_zn(evalData{r}, model_2_phi);
-        [~, ~, Loglik2] = LogForwardBackward(logp_xn_given_znt, model_2_p_start, model_2_A);
+        logp_xn_given_zn = Gmm_logp_xn_given_zn(evalData{r}, model_l{3});
+        [~, ~, Loglik1] = LogForwardBackward(logp_xn_given_zn, model_l{1}, model_l{2});
+        logp_xn_given_znt = Gmm_logp_xn_given_zn(evalData{r}, model_2{3});
+        [~, ~, Loglik2] = LogForwardBackward(logp_xn_given_znt, model_2{1}, model_2{2});
     end
+    disp(Loglik1)
+    disp(Loglik2)
     if Loglik2 > Loglik1
         disp("2")
     end
@@ -130,4 +140,40 @@ function dataInMatrix = audioProcessing(soundToAnalyse, fs)
     rolloffPoint = spectralRolloffPoint(soundToAnalyse,fs);
     flatness = spectralFlatness(soundToAnalyse,fs);
     dataInMatrix = [f0, hr,centroid,flux,rolloffPoint,flatness];
+end
+%Should take the array of sound data as input
+function modelMatrix = trainModels(audioData)
+    addpath(genpath("./matlab-hmm-master"))
+    dataSize = size(audioData);
+    GmmModels = cell(dataSize(1), 1); 
+    for i =1:dataSize(1)
+        % GMM and HMM implementation calls
+        Q = 6;      % state num
+        M = 3;      % mix num
+        [p_start, A, phi, ~] = ChmmGmm(audioData(i), Q, M);
+        GmmModels(i, 1) = {Model(p_start, A, phi)};
+        disp("current model done")
+        disp(num2str(i))
+    end
+    disp("All models done")
+    modelMatrix = GmmModels;
+end
+
+function loglikMatrix = evalModels(evalData, modelMatrix)
+    obj_num = length(evalData);
+    addpath(genpath("./matlab-hmm-master"))
+    dataSize = size(modelMatrix);
+    loglikOnModels = cell(dataSize(1), 1); 
+    for i =1:dataSize(1)
+        for r = 1:obj_num
+            logp_xn_given_zn = Gmm_logp_xn_given_zn(evalData{r}, modelMatrix{i}.phi);
+            [~, ~, Loglik] = LogForwardBackward(logp_xn_given_zn, modelMatrix{i}.p_start, modelMatrix{i}.A);
+        end
+        loglikOnModels(i, 1) = {Loglik};
+        disp("eval current model done")
+        disp(num2str(i))
+    end
+    disp("All models done")
+    loglikMatrix = loglikOnModels;
+    
 end
