a916ea9cf07870c6fcc57cac422284423b33e68f
iRandz <chrbj-@hotmail.com>
2019-04-25 13:08:57 +0200

    File Retrieval - not done still needs to be looped through

--- P4_AP.m
+++ P4_AP.m
@@ -1,8 +1,11 @@
 %% AudioProcessing
+clc
+clear
+
 addpath(genpath("./audio"))
-[Help1,Fs] = audioread('10_hjï¿½lp_Mathias.wav');
+[Help1,Fs] = audioread('10_hjælp_Mathias.wav');
 [Help2,Fs] = audioread('help-Glerup.wav');
-[Help3,Fs] = audioread('Hjï¿½lp_Rasmus.wav');
+[Help3,Fs] = audioread('Hjælp_Rasmus.wav');
 [Help4,Fs] = audioread('10_helps_Female.wav');
 [skrig1,Fs] = audioread('10_skrig_Mathias.wav');
 [skrig2,Fs] = audioread('screech-Glerup.wav');
@@ -16,18 +19,22 @@ framesize = 30/1000*fs;
 % Assumed implementation of loop : help
 % move into function later
 h = [];
-% loop through folder with different people
-for i = 0:foldersize1
-    foldersize2 = size(folder, i);
-    % loop through folder of different sound files and add parameters to
-    % list.
-    for j = 0:foldersize2
-        [Help,Fs] = audioread('file[j]');
-        h[i] = append(h[i], audioProcessing(Help, Fs));
-    end
-    % create different combinations of parameters
-
+% retrieve names of individual files in folders
+% Define a starting folder.
+start_path = fullfile(matlabroot, '\toolbox');
+if ~exist(start_path, 'dir')
+	start_path = matlabroot;
 end
+% Ask user to confirm the folder, or change it.
+uiwait(msgbox('Pick a starting folder on the next window that will come up.'));
+topLevelFolder = uigetdir(start_path);
+if topLevelFolder == 0
+	return;
+end
+fprintf('The top level folder is "%s".\n', topLevelFolder);
+names = fileRetrieve(topLevelFolder);
+
+[Help1,Fs] = audioread(names(1));
 
 % Do AP on help signals
 h1 = audioProcessing(Help1, Fs);
