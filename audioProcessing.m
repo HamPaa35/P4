018cab42ddf510d77495a805ed533870bcd43582
iRandz <chrbj-@hotmail.com>
2019-04-30 14:05:11 +0200

    Moved the AP methods into their own file so that they could be accessed from the evaluation code.

--- audioProcessing.m
+++ audioProcessing.m
@@ -3,7 +3,7 @@ function res = audioProcessing()
     combNumb = 6;
     colStart = combNumb +1;
     soundTypes = 3;
-    allAudioRes = cell(combNumb, soundTypes);
+    allAudioRes = cell(combNumb*2-1, soundTypes);
     for k = 1:soundTypes
         start_path = fullfile("./audio");
         if ~exist(start_path, 'dir')
@@ -32,83 +32,21 @@ function res = audioProcessing()
         % Do AP on retrieved audio files
         for i = 1:length(filePaths)
             [Help,Fs] = audioread(filePaths(i));
-            allAudioRes{1,k}{i} = audioProcessingNorm(Help, Fs);
-%             allAudioRes{1,k}{i} = audioProcessingRaw(Help, Fs);
+            allAudioRes{1,k}{i} = aPMethod(Help, Fs);
         end
         % Split AP results into different combinations
         for j = 2:combNumb
             allAudioRes{j, k} = allAudioRes{1,k};
+            row = j+5;
             for i = 1:length(allAudioRes{1,k})
-%                 if j > 1 % Check if this can be removed!
+                allAudioRes{row, k}{i} = allAudioRes{1, k}{i}(:,j);
                     for g = 1:j-1
                         allAudioRes{j, k}{i}(:, colStart-g) = [];
                     end
-%                 end
             end
         end
+        
     end
     res = allAudioRes;
     disp("done")
-end
-
-% Function for running all AP methods on a signal and returning them in a
-% matrix.
-function dataInMatrix = audioProcessingRaw(soundToAnalyse, fs)
-    a = 0; % minimum value after mapping
-    b = 1; % maximum value after mapping
-    % Converts Stereo signals into mono signals.
-    if size(soundToAnalyse,2)>1
-        soundToAnalyse= sum(soundToAnalyse, 2) / size(soundToAnalyse, 2);
-    end
-    % Harmonic to noise ratio
-    hr = harmonicRatio(soundToAnalyse, fs);
-    % Pitch
-    fTemp = pitch(soundToAnalyse, fs);
-    hrSize = size(hr);
-    fTempSize = size(fTemp);
-    sizeDifference = hrSize(1) - fTempSize(1);
-    fzeros = zeros(sizeDifference, hrSize(2));
-    f0 = [fTemp;fzeros];
-    % Spectral centroid
-    centroid = spectralCentroid(soundToAnalyse, fs);
-    %Flux
-    flux = spectralFlux(soundToAnalyse,fs);
-    % Roll off Point
-    rolloffPoint = spectralRolloffPoint(soundToAnalyse,fs);
-    % Spectral flatness
-    flatness = spectralFlatness(soundToAnalyse,fs);
-    % Output
-    dataInMatrix = [f0, hr,centroid,flux,rolloffPoint,flatness];
-end
-% AP with normalization
-function dataInMatrix = audioProcessingNorm(soundToAnalyse, fs)
-    % Converts Stereo signals into mono signals.
-    if size(soundToAnalyse,2)>1
-        soundToAnalyse= sum(soundToAnalyse, 2) / size(soundToAnalyse, 2);
-    end
-    % Harmonic to noise ratio
-    hr = harmonicRatio(soundToAnalyse, fs);
-    % Pitch
-    fTemp = pitch(soundToAnalyse, fs);
-    hrSize = size(hr);
-    fTempSize = size(fTemp);
-    sizeDifference = hrSize(1) - fTempSize(1);
-    fzeros = zeros(sizeDifference, hrSize(2));
-    f0 = [fTemp;fzeros];
-    f0 = f0/(fs/2);
-    % Spectral centroid
-    centroid = spectralCentroid(soundToAnalyse, fs);
-    centroid = centroid/(fs/2);
-    %Flux
-%     flux = spectralFlux(soundToAnalyse, fs);
-%     flux = flux*10;
-    [s,cf,t] = melSpectrogram(soundToAnalyse,fs);
-    flux = spectralFlux(s,cf);
-    % Roll off Point
-    rolloffPoint = spectralRolloffPoint(soundToAnalyse,fs);
-    rolloffPoint = rolloffPoint/(fs/2);
-    % Spectral flatness
-    flatness = spectralFlatness(soundToAnalyse,fs);
-    % Output
-    dataInMatrix = [f0, hr,centroid,flux,rolloffPoint,flatness];
 end
