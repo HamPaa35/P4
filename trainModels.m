422c84e8056c2941a7f75dfd866cfd6e7beef42e
HamPaa35 <mfaldt17@student.aau.dk>
2019-05-01 10:43:03 +0200

    Changed parameters

--- trainModels.m
+++ trainModels.m
@@ -5,7 +5,7 @@ function modelMatrix = trainModels(audioData)
     GmmModels = cell(dataSize(1), 1);
     for i = 1:dataSize(2)
         % GMM and HMM implementation calls
-        Q = 6;      % state num %Antal stavelser
+        Q = 30;      % state num %Antal stavelser
         M = 3;      % mix num %Gausians per stavelser %Ikke mega vigtigt, men prøv lidt
         [p_start, A, phi, ~] = ChmmGmm(audioData{i}, Q, M);
         GmmModels(i, 1) = {Model(p_start, A, phi)};
