ddc049a3100e3388fa87bd394b8986f9804b4841
HamPaa35 <mfaldt17@student.aau.dk>
2019-05-03 14:03:29 +0200

    The code can now do some fancy eval

--- P4_AP.m
+++ P4_AP.m
@@ -1,5 +1,6 @@
 %% Retrieval of training data
-h = dataRetrieval();
+% 1 = scream 2 = help 3 = fall
+e = dataRetrieval();
 %% Train Gmm-Hmm model
 % for i = 7:11
 %     Models{i} = trainModels(h,i);
@@ -13,6 +14,29 @@ h = dataRetrieval();
 % Models9 = trainModels(h,9);
 % Models10 = trainModels(h,10);
 % Models11 = trainModels(h,11);
+%% Eval test
+modelRow = 11;
+OverallSize = size(e);
+for j = 1:OverallSize(2)
+    smallerSize = size(e{modelRow,j});
+    for i = 1:smallerSize(2)
+        [~, bestModel] = evalModels(e{modelRow, j}(1, i), Models11);
+        z11(j,i) = bestModel;
+    end
+end
+disp('du ser godt ud')
+
+%% Optælling
+OverallSize = size(z6);
+res = 0;
+for j = 1:OverallSize(1)
+    for i = 1:OverallSize(2)
+        if j == z11(j, i)
+            res=res++1;
+        end
+    end
+end
+res11 = res;
 %% Eval model vs. data
 addpath(genpath("./audio"))
 [HelpTest,Fs] = audioread('Hjælp_GMM_refinement.wav');
