8fce5e9570c6c43d39c10595b7c94ecb0f67449b
HamPaa35 <mfaldt17@student.aau.dk>
2019-05-03 09:11:17 +0200

    Made small changes to make the training easier

--- P4_AP.m
+++ P4_AP.m
@@ -1,10 +1,21 @@
 %% Retrieval of training data
 h = dataRetrieval();
 %% Train Gmm-Hmm model
-GmmModels = trainModels(h);
+% for i = 7:11
+%     Models{i} = trainModels(h,i);
+%     disp("done")
+%     disp(i)
+% end
+% AllGmm = trainModels(h,1);
+% Models8 = trainModels(h,8);
+% Models6 = trainModels(h,6);
+% Models7 = trainModels(h,7);
+% Models9 = trainModels(h,9);
+% Models10 = trainModels(h,10);
+% Models11 = trainModels(h,11);
 %% Eval model vs. data
 addpath(genpath("./audio"))
-[HelpTest,Fs] = audioread('HjÃ¦lp_GMM_refinement.wav');
+[HelpTest,Fs] = audioread('Hjælp_GMM_refinement.wav');
 [skrigTest,Fs] = audioread('Skrig_GMM_refinement.wav');
 recObj = audiorecorder(8000, 16, 1);
 
@@ -13,10 +24,10 @@ recordblocking(recObj, 2);
 disp('End of Recording.');
 
 liveData = getaudiodata(recObj, 'double');
-Live = {audioProcessingNorm(liveData, 8000)};
+Live = {audioProcessing(liveData, 8000)};
 
-ht = {audioProcessingNorm(HelpTest, 8000)};
+ht = {audioProcessing(HelpTest, 8000)};
 
-st = {audioProcessingNorm(skrigTest, 8000)};
+st = {audioProcessing(skrigTest, 8000)};
 
 [loglikFromAllModels, bestModel] = evalModels(Live, GmmModels);
